bf80203c47642542c6f2e60a917ca60d2d45cf2d
jonnyzar <64309336+jonnyzar@users.noreply.github.com>
2020-10-30 10:53:33 +0100

    still looking how to crawl though directories

--- dircrawler.cpp
+++ dircrawler.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 //list all directories and write to file
-void write_dirs_to_file(const char *dirname, FILE *dirfile);
+void write_dirs_to_file(const char *dirname);
 
 int main(int argc, char **argv)
 {
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
     char somepath[] = "contents.txt";
     pFile = fopen(somepath,"w+");
 
-    write_dirs_to_file("/",pFile);
+    write_dirs_to_file("/usr");//,Pfile
 
     fclose(pFile);
 
@@ -28,35 +28,21 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void write_dirs_to_file(const char *dirname, FILE *dirfile){
+void write_dirs_to_file(const char *dirname){//, FILE *dirfile
 
     /* Print all files and directories within the directory */
     DIR *dir;
 
     dir = opendir(dirname);
-	struct dirent *ent;
+	struct dirent *ent; 
+
 	while ((ent = readdir(dir)) != NULL) {	
+        cout << ent->d_name << endl;
 
-        switch (ent->d_type) {
-		case DT_REG:
-			fputs("f ",dirfile);
-			break;
-
-		case DT_DIR:
-			fputs("d ",dirfile);            
-            //write_dirs_to_file(ent->d_name,dirfile);
-			break;
-
-		case DT_LNK:
-			fputs("l ",dirfile);
-			break;
-
-		default:
-			fputs("o ",dirfile);
-		}
-                
-        fputs(ent->d_name,dirfile);
-        fputs(" \n",dirfile);
+        
+               
+       // fputs(ent->d_name,dirfile);
+       // fputs(" \n",dirfile);
 		
 	}
 
